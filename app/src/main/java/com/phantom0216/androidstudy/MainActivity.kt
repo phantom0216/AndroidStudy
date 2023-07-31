25b3ac7ee912f041e1dc71c510abb88937d7d1f2
ernestzhu <ernestzhu@tencent.com>
2023-07-31 23:31:51 +0800

    框架

--- app/src/main/java/com/phantom0216/androidstudy/MainActivity.kt
+++ app/src/main/java/com/phantom0216/androidstudy/MainActivity.kt
@@ -2,10 +2,33 @@ package com.phantom0216.androidstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.phantom0216.androidstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mViewBinding: ActivityMainBinding
+    private lateinit var mAdapter: MainActivityAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mViewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mViewBinding.root)
+        initViews()
+        bindData()
+    }
+
+    private fun initViews() {
+        mViewBinding.recyclerView.layoutManager = LinearLayoutManager(this)
+        mAdapter = MainActivityAdapter()
+        mViewBinding.recyclerView.adapter = mAdapter
+    }
+
+    private fun bindData() {
+        val items = mutableListOf<String>()
+        items.add("recyclerView")
+        items.add("协程")
+
+        mAdapter.onDataSetChanged(items)
     }
 }
---
+++ app/src/main/java/com/phantom0216/androidstudy/MainActivityAdapter.kt
@@ -0,0 +1,39 @@
+package com.phantom0216.androidstudy
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.phantom0216.androidstudy.databinding.MainListItemLayoutBinding
+
+class MainActivityAdapter : RecyclerView.Adapter<MainActivityAdapter.ItemViewHolder>() {
+
+    private val mItems = mutableListOf<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val viewBinding =
+            MainListItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(viewBinding)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bindData(mItems[position])
+    }
+
+    fun onDataSetChanged(items: List<String>) {
+        mItems.clear()
+        mItems.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return mItems.size
+    }
+
+    class ItemViewHolder(private val viewBinding: MainListItemLayoutBinding) :
+        RecyclerView.ViewHolder(viewBinding.root) {
+
+        fun bindData(title: String) {
+            viewBinding.root.text = title
+        }
+    }
+}
